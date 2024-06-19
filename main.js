8494f5bc460bc727a4584f5f74afd4e29badf220
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-06-19 19:11:35 +0000

    Add js file with main variable for questions

---
+++ main.js
@@ -0,0 +1,23 @@
+const questions=[{
+    question:'Who won the Tour the France in 2021?',
+    answers:[
+
+        { text: 'Tadej Pogacar',correct:true},
+        { text: '',correct:false},
+        { text: '',correct:false},
+        { text: '',correct:false},
+    ]
+},
+{
+    question:'Who won the Tour the France in 2021?',
+    answers:[
+
+        { text: 'Tadej Pogacar',correct:true},
+        { text: '',correct:false},
+        { text: '',correct:false},
+        { text: '',correct:false},
+    ]
+},
+
+
+]
