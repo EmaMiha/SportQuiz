91cc97dbd871c61a7fbe5feafad645a5efb59b67
Milena Ceglia Mihajilovic <ema.mihajilovic@gmail.com>
2024-07-01 16:27:56 +0000

    Add code for improving functionality of application

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,146 +8,146 @@ const questions = [{
     { text: 'Egan Bernal', correct: false },
   ]
 },
-{
-  question: 'Who is the all-time leading scorer in the history of the UEFA Champions League?',
-  answers: [
+// {
+//   question: 'Who is the all-time leading scorer in the history of the UEFA Champions League?',
+//   answers: [
 
-    { text: 'Lionel Messi', correct: false },
-    { text: 'Raúl González', correct: false },
-    { text: 'Cristiano Ronaldo', correct: true },
-    { text: 'Robert Lewandowski', correct: false },
-  ]
-},
-{
-  question: 'Who won the UEFA Euro 2020 tournament?',
-  answers: [
+//     { text: 'Lionel Messi', correct: false },
+//     { text: 'Raúl González', correct: false },
+//     { text: 'Cristiano Ronaldo', correct: true },
+//     { text: 'Robert Lewandowski', correct: false },
+//   ]
+// },
+// {
+//   question: 'Who won the UEFA Euro 2020 tournament?',
+//   answers: [
 
-    { text: 'France', correct: false },
-    { text: 'Croatia', correct: false },
-    { text: 'Portugal', correct: false },
-    { text: 'Italy', correct: true },
-  ]
-},
-{
-  question: 'Who won the 2022 NBA MVP award?',
-  answers: [
+//     { text: 'France', correct: false },
+//     { text: 'Croatia', correct: false },
+//     { text: 'Portugal', correct: false },
+//     { text: 'Italy', correct: true },
+//   ]
+// },
+// {
+//   question: 'Who won the 2022 NBA MVP award?',
+//   answers: [
 
-    { text: 'Nikola Jokic', correct: true },
-    { text: 'LeBron James', correct: false },
-    { text: 'Kevin Durant', correct: false },
-    { text: 'Stephen Curry', correct: false },
-  ]
-},
-{
-  question: ' What is the only country to have played in every World Cup soccer tournament?',
-  answers: [
+//     { text: 'Nikola Jokic', correct: true },
+//     { text: 'LeBron James', correct: false },
+//     { text: 'Kevin Durant', correct: false },
+//     { text: 'Stephen Curry', correct: false },
+//   ]
+// },
+// {
+//   question: ' What is the only country to have played in every World Cup soccer tournament?',
+//   answers: [
 
-    { text: 'Argentina', correct: false },
-    { text: 'Germany', correct: false },
-    { text: 'France', correct: false },
-    { text: 'Brazil', correct: true },
-  ]
-},
-{
-  question: 'Which country has won the most medals in Olympic snowboarding history?',
-  answers: [
+//     { text: 'Argentina', correct: false },
+//     { text: 'Germany', correct: false },
+//     { text: 'France', correct: false },
+//     { text: 'Brazil', correct: true },
+//   ]
+// },
+// {
+//   question: 'Which country has won the most medals in Olympic snowboarding history?',
+//   answers: [
 
-    { text: 'Switzerland', correct: false },
-    { text: 'United States', correct: true },
-    { text: 'Norway', correct: false },
-    { text: 'Canada', correct: false },
-  ]
-},
-{
-  question: "Who won the Men's Singles title at the Australian Open in 2023?",
-  answers: [
+//     { text: 'Switzerland', correct: false },
+//     { text: 'United States', correct: true },
+//     { text: 'Norway', correct: false },
+//     { text: 'Canada', correct: false },
+//   ]
+// },
+// {
+//   question: "Who won the Men's Singles title at the Australian Open in 2023?",
+//   answers: [
 
-    { text: 'Rafael Nadal', correct: false },
-    { text: 'Dominic Thiem', correct: false },
-    { text: 'Novak Djokovic', correct: true },
-    { text: 'Daniil Medvedev', correct: false },
-  ]
-},
-{
-  question: "Who is hosting the UEFA Women's Euro 2025?",
-  answers: [
+//     { text: 'Rafael Nadal', correct: false },
+//     { text: 'Dominic Thiem', correct: false },
+//     { text: 'Novak Djokovic', correct: true },
+//     { text: 'Daniil Medvedev', correct: false },
+//   ]
+// },
+// {
+//   question: "Who is hosting the UEFA Women's Euro 2025?",
+//   answers: [
 
-    { text: 'Italy', correct: false },
-    { text: 'Germany', correct: false },
-    { text: 'Serbia', correct: false },
-    { text: 'Switzerland', correct: true },
-  ]
-},
-{
-  question: 'Who is the greatest driver F1 in history?',
-  answers: [
+//     { text: 'Italy', correct: false },
+//     { text: 'Germany', correct: false },
+//     { text: 'Serbia', correct: false },
+//     { text: 'Switzerland', correct: true },
+//   ]
+// },
+// {
+//   question: 'Who is the greatest driver F1 in history?',
+//   answers: [
 
-    { text: 'Lewis Hamilton', correct: true },
-    { text: 'Max Verstappen', correct: false },
-    { text: 'Michael Schumacher', correct: false },
-    { text: 'Fernando Alonso', correct: false },
-  ]
-},
-{
-  question: 'What is the deepest Olympic diving pool?',
-  answers: [
+//     { text: 'Lewis Hamilton', correct: true },
+//     { text: 'Max Verstappen', correct: false },
+//     { text: 'Michael Schumacher', correct: false },
+//     { text: 'Fernando Alonso', correct: false },
+//   ]
+// },
+// {
+//   question: 'What is the deepest Olympic diving pool?',
+//   answers: [
 
-    { text: '7 meters', correct: false },
-    { text: '5 meters', correct: true },
-    { text: '3 meters', correct: false },
-    { text: '4 meters', correct: false },
-  ]
-},
-{
-  question: 'Where did the first modern Summer Olympic games take place in 1896?',
-  answers: [
+//     { text: '7 meters', correct: false },
+//     { text: '5 meters', correct: true },
+//     { text: '3 meters', correct: false },
+//     { text: '4 meters', correct: false },
+//   ]
+// },
+// {
+//   question: 'Where did the first modern Summer Olympic games take place in 1896?',
+//   answers: [
 
-    { text: 'Australia', correct: false },
-    { text: 'Denmark', correct: false },
-    { text: 'Italy', correct: false },
-    { text: 'Greece', correct: true },
-  ]
-},
-{
-  question: 'Which French city is known for its 24 hour motor race?',
-  answers: [
+//     { text: 'Australia', correct: false },
+//     { text: 'Denmark', correct: false },
+//     { text: 'Italy', correct: false },
+//     { text: 'Greece', correct: true },
+//   ]
+// },
+// {
+//   question: 'Which French city is known for its 24 hour motor race?',
+//   answers: [
 
-    { text: 'Lyon', correct: false },
-    { text: 'Marseille', correct: false },
-    { text: 'Le Mans', correct: true },
-    { text: 'Bordeaux', correct: false },
-  ]
-},
-{
-  question: 'Which team won the NBA Championship in 2022?',
-  answers: [
+//     { text: 'Lyon', correct: false },
+//     { text: 'Marseille', correct: false },
+//     { text: 'Le Mans', correct: true },
+//     { text: 'Bordeaux', correct: false },
+//   ]
+// },
+// {
+//   question: 'Which team won the NBA Championship in 2022?',
+//   answers: [
 
-    { text: 'Golden State Warriors', correct: true },
-    { text: 'Boston Celtics', correct: false },
-    { text: 'Denver Nuggets', correct: false },
-    { text: 'Lakers', correct: false },
-  ]
-},
-{
-  question: 'Which country has won the most Olympic medals in sailing?',
-  answers: [
+//     { text: 'Golden State Warriors', correct: true },
+//     { text: 'Boston Celtics', correct: false },
+//     { text: 'Denver Nuggets', correct: false },
+//     { text: 'Lakers', correct: false },
+//   ]
+// },
+// {
+//   question: 'Which country has won the most Olympic medals in sailing?',
+//   answers: [
 
-    { text: 'Brazil', correct: false },
-    { text: 'Spain', correct: false },
-    { text: 'Great Britain', correct: true },
-    { text: 'United States', correct: false },
-  ]
-},
-{
-  question: 'What country has won the most World Cup soccer championships?',
-  answers: [
+//     { text: 'Brazil', correct: false },
+//     { text: 'Spain', correct: false },
+//     { text: 'Great Britain', correct: true },
+//     { text: 'United States', correct: false },
+//   ]
+// },
+// {
+//   question: 'What country has won the most World Cup soccer championships?',
+//   answers: [
 
-    { text: 'Brazil', correct: true },
-    { text: 'Argentina', correct: false },
-    { text: 'France', correct: false },
-    { text: 'Portugal', correct: false },
-  ]
-},
+//     { text: 'Brazil', correct: true },
+//     { text: 'Argentina', correct: false },
+//     { text: 'France', correct: false },
+//     { text: 'Portugal', correct: false },
+//   ]
+// },
 {
   question: 'Who is the greatest Boxer in history?',
   answers: [
@@ -210,8 +210,9 @@ const answers = document.querySelector(".answers");
 const score = document.querySelector(".score");
 const result = document.querySelector("#results")
 const previousS = document.querySelector("#previousScores");
-const feedback=document.querySelector(".feedback");
-const restart=document.querySelector(".restartQuiz");
+const feedback = document.querySelector(".feedback");
+const restart = document.querySelector(".restartQuiz");
+
 
 let username = '';
 let currentQuestionIndex;
@@ -219,7 +220,7 @@ let pastQuestions = [];
 let correctAnswer = 0;
 
 
-restart.addEventListener("click",restartQuiz);
+restart.addEventListener("click", restartQuiz);
 startQuizButton.addEventListener("click", startQuiz);
 
 /**
@@ -231,6 +232,7 @@ function startQuiz() {
   username = inputUsername.value.trim();
 
   if (username) {
+    pastQuestions = [];
     localStorage.setItem('username', username);
     login.style.display = "none";
     quizC.style.display = "block";
@@ -246,6 +248,8 @@ function startQuiz() {
 
 
 function nextQuestion(n) {
+  
+  feedback.textContent = "";
   if (n != 1) {
 
     let rb = document.querySelectorAll("input[type='radio']:checked");
@@ -260,12 +264,13 @@ function nextQuestion(n) {
   let rand;
 
   if (pastQuestions.length == questions.length) {
-    feedback.textContent="No more questions!Please enter username for another game!"
-    quizC.style.display="none";
-
-    score.textContent = "Final -> "+ correctAnswer;
+    inputUsername.value='';
+    feedback.textContent = "No more questions!Please enter username for another game!"
+    quizC.style.display = "none";
+    restart.style.display="none";
+    score.textContent = correctAnswer;
     result.style.display = 'block';
-    login.style.display="block";
+    login.style.display = "block";
     let previousScore = JSON.parse(localStorage.getItem("previous"));
     let user = localStorage.getItem('username');
     previousScore.push({ user: user, score: correctAnswer });
@@ -295,8 +300,18 @@ function nextQuestion(n) {
 
 
 function showQuestion(rand) {
+  restart.style.display="block";
+         
+  let rbut=document.querySelectorAll(".rbut");
+  if (rbut){
+  rbut.forEach(elem=>{
+    elem.disabled=false;
+  })
+}
 
-  question.textContent = questions[rand].question;
+
+
+  question.textContent = pastQuestions.length+"."+" " +questions[rand].question;
 
   let num = 0;
   answers.textContent = "";
@@ -308,18 +323,30 @@ function showQuestion(rand) {
     rbutton.type = "radio";
     rbutton.name = "quiz";
     rbutton.value = num++;
-    rbutton.id = "tt" + num++;
+    rbutton.classList.add("rbut");
+    rbutton.id = "tt" + num;
 
 
     rbutton.addEventListener("change", function () {
+       
+      let rbut=document.querySelectorAll(".rbut");
+      rbut.forEach(elem=>{
+        elem.disabled=true;
+      })
 
 
       if (answer.correct == true) {
 
-        feedback.textContent="Correct answer!!!!"
+        let img = document.createElement("img");
+        img.src = "assets/img/correct.png";
+        img.alt = "correct";
+        img.style.width="30px";
+        img.style.height="30px";
+        feedback.appendChild(img);
         rbutton.nextElementSibling.style.color = "green";
         correctAnswer++;
-        score.textContent=correctAnswer.toString();
+
+        score.textContent = correctAnswer.toString();
         setTimeout(function () {
 
 
@@ -329,8 +356,14 @@ function showQuestion(rand) {
 
 
       } else {
-         feedback.textContent= `Wrong  answer :( `;
-         score.textContent=correctAnswer.toString();
+        let img = document.createElement("img");
+        img.src = "assets/img/wrong.png";
+        img.alt = "correct";
+        img.style.width="30px";
+        img.style.height="30px";
+        feedback.appendChild(img);
+
+        score.textContent = correctAnswer.toString();
         rbutton.nextElementSibling.style.color = "red"
         setTimeout(function () {
 
@@ -342,16 +375,22 @@ function showQuestion(rand) {
     })
 
     const label = document.createElement("label");
-    label.classList.add(`label${num++}`);
-    label.htmlFor = "tt" + num++;
-    label.textContent = answer.text;
-
+    label.classList.add(`label${num}`);
+    label.htmlFor = "tt" + num;
+    label.textContent =answer.text;
+     num++;
 
     container.appendChild(rbutton);
     container.appendChild(label);
 
 
+
+
     answers.appendChild(container);
+
+
+  
+
 
   })
 }
@@ -362,6 +401,7 @@ function showQuestion(rand) {
 
 function init() {
   let previousScore = JSON.parse(localStorage.getItem("previous"));
+  restart.style.display="none";
 
   if (!previousScore) {
     localStorage.setItem("previous", JSON.stringify([]));
@@ -382,7 +422,7 @@ function writePreviousScore(previousScore) {
   if (previousScore.length == 0) {
     previousS.textContent = "No results!";
     return;
-  } 
+  }
 
   let html = `<table>
 <tr>
@@ -406,10 +446,17 @@ function writePreviousScore(previousScore) {
 }
 
 
-function restartQuiz(){
+function restartQuiz() {
 
-  correctAnswer=0;
-  login.style.display="block";
-  quizC.style.display="none";
+  let b=confirm("Are you sure you want to start quiz again?");
+  if(b==false){
+
+    return;
+  }
+
+  correctAnswer = 0;
+  score.textContent = correctAnswer;
+  pastQuestions=[];
+  nextQuestion(1);
 
 }
